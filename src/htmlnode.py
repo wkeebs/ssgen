3220417bd3c5d512f0c8bada381bceed35a36c5b
Will Keeble <will.a.keeble@gmail.com>
2024-11-05 12:36:47 +1100

    add documentation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,18 +1,59 @@
+from typing import List, Self, Dict
+
 
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=None, props=None):
+    def __init__(self, tag: str = None, value: str = None, children: List[Self] = None, props: str = None) -> None:
+        """
+        : @summary :
+        Represents a general HTML node
+        ___________________
+
+        : @args :
+            * tag (str, optional): the tag type of the node (e.g., "a" for <a>). Defaults to None.
+            * value (str, optional): the value inside the node (e.g., <p>value here</p>). Defaults to None.
+            * children (HTMLNode[], optional): any nested child nodes. Defaults to None.
+            * props {str: str}, optional): the properties of the node. Defaults to None.
+        ___________________
+        """
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        : @summary :
+        String representation of the node
+        ___________________
+
+        : @returns : 
+            * str: the repr
+        ___________________
+        """
         return f"HTMLNode | Tag: '{self.tag}', Value: '{self.value}', Children: '{self.children}', Props: '{self.props}'"
 
-    def to_html(self):
+    def to_html(self) -> NotImplementedError:
+        """
+        : @summary :
+        To be overridden by children
+        ___________________
+
+        : @raises :
+            * NotImplementedError: when called directly
+        ___________________
+        """
         raise NotImplementedError
 
-    def props_to_html(self):
+    def props_to_html(self) -> str:
+        """
+        : @summary :
+        Converts the properties of the HTML node to string format
+        ___________________
+
+        : @returns : 
+            * str: the formatted properties
+        ___________________
+        """
         if self.props is None:
             return ""
 
@@ -23,10 +64,34 @@ class HTMLNode:
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
+    def __init__(self, tag: str = None, value: str = None, props: Dict[str][str] = None) -> None:
+        """
+        : @summary :
+        Represents a leaf node, i.e., a HTML node with no children
+        ___________________
+
+        : @args :
+            * tag (str, optional): the HTML tag. Defaults to None.
+            * value (str, optional): the value inside the leaf. Defaults to None.
+            * props ({str: str}, optional): any properties the node has. Defaults to None.
+        ___________________
+        """
         super().__init__(tag, value, None, props)
 
-    def to_html(self):
+    def to_html(self) -> str:
+        """
+        : @summary :
+        Converts the leaf node to HTML format
+        ___________________
+
+        : @raises :
+            * ValueError: when the leaf has no value
+        ___________________
+
+        : @returns : 
+            * str: the formatted node (i.e., the repr)
+        ___________________
+        """
         if self.value is None:
             raise ValueError("All leaf nodes must have a value")
 
@@ -39,10 +104,36 @@ class LeafNode(HTMLNode):
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag=None, children=[], props=None):
+    def __init__(self, tag: str = None, children: List[HTMLNode] = [], props: Dict[str][str] = None) -> None:
+        """
+        : @summary :
+        Represents a "Parent" HTML node - one that has some nested children
+        ___________________
+
+        : @args :
+            * tag (str, optional): the HTML tag. Defaults to None.
+            * children (HTMLNode[], optional): any child nodes. Defaults to [].
+            * props ({str: str}, optional): the HTML properties. Defaults to None.
+        ___________________
+        """
         super().__init__(tag, None, children, props)
 
-    def to_html(self):
+    def to_html(self) -> str:
+        """
+        : @summary :
+        Converts the parent node to its html format, recursively converting
+        its children in the process.
+        ___________________
+
+        : @raises :
+            * ValueError: when the parent has no tag
+            * ValueError: when the parents has no children
+        ___________________
+
+        : @returns : 
+            * str: the HTML formatted node
+        ___________________
+        """
         if self.tag is None:
             raise ValueError("All parent nodes must have a tag")
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,15 @@
 from enum import Enum
+from typing import Self
 
 from htmlnode import LeafNode
 
 
 class TextType(Enum):
+    """
+    : @summary :
+    Enumerator for @TextNode, specifically the type of node
+    ___________________
+    """
     TEXT = "text"
     PARAGRAPH = "paragraph"
     BOLD = "bold"
@@ -14,23 +20,73 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text: str, text_type: TextType, url: str = None):
+    def __init__(self, text: str, text_type: TextType, url: str = None) -> None:
+        """
+        : @summary :
+        Represents a base text HTML node
+        ___________________
+
+        : @args :
+            * text (str): the inner text
+            * text_type (TextType): the type of node
+            * url (str, optional): the url (if applicable). Defaults to None.
+        ___________________
+        """
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, other):
+    def __eq__(self, other: Self) -> bool:
+        """
+        : @summary :
+        Textnodes are equal if all properties match
+        ___________________
+
+        : @args :
+            * other (TextNode): the comparing node
+        ___________________
+
+        : @returns : 
+            * bool: do they match?
+        ___________________
+        """
         return (
             self.text == other.text
             and self.text_type == other.text_type
             and self.url == other.url
         )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        : @summary :
+        String representation generator
+        ___________________
+
+        : @returns : 
+            * str: the repr
+        ___________________
+        """
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
 def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    """
+    : @summary :
+    Converts a given TextNode to a LeafNode, based on the node type
+    ___________________
+
+    : @args :
+        * text_node (TextNode): the node itself
+    ___________________
+
+    : @raises :
+        * Exception: when an invalid text type is given
+    ___________________
+
+    : @returns : 
+        * LeafNode: the converted node
+    ___________________
+    """
     match text_node.text_type:
         case TextType.TEXT:
             return LeafNode(value=text_node.text)
