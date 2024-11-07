a1a73cc0682a090537f15a071dce6d2dc39f7a67
Will Keeble <will.a.keeble@gmail.com>
2024-11-07 18:42:40 +1100

    create directory copying

---
+++ src/copy_contents.py
@@ -0,0 +1,80 @@
+import os
+import shutil
+
+
+def copy_dir_contents(src: str, dest: str) -> bool:
+    """
+    : @summary :
+    Copies the contents from one directory to another,
+    overwriting the old content.
+    ___________________
+
+    : @args :
+        * src (str): the source path
+        * dest (str): the destination path
+    ___________________
+
+    : @returns : 
+        * bool: success?
+    ___________________
+    """
+    # validate paths
+    if not os.path.exists(src):
+        print(f"Source does not exist: {src}")
+        return False
+
+    if not os.path.exists(dest):
+        print(f"Destination does not exist: {dest}")
+        return False
+
+    print(f"Cleaning {dest} ...")
+
+    # clean the destination directory
+    for filename in os.listdir(dest):
+        file_path = os.path.join(dest, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            return False
+
+    print(f"{dest} successfully cleaned")
+    print(f"Copying {src} into {dest} ...")
+    success = copy_contents(src, dest)
+    return success
+
+
+def copy_contents(src: str, dir: str) -> bool:
+    """
+    : @summary :
+    Copies one directory into another recursively.
+    ___________________
+
+    : @args :
+        * src (str): the source path
+        * dir (str): the destination path
+    ___________________
+
+    : @returns : 
+        * bool: success?
+    ___________________
+    """
+    for filename in os.listdir(src):
+        file_path = os.path.join(src, filename)
+        try:
+            new_path = os.path.join(dir, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                print(f"File: {file_path} -> {new_path}")
+                shutil.copy(file_path, new_path)
+            elif os.path.isdir(file_path):
+                print(f"Dir: {new_path} -> {new_path}")
+                new_dir = dir + filename
+                os.mkdir(new_dir)
+                copy_contents(file_path, new_dir)
+        except Exception as e:
+            print('Failed to copy in %s. Reason: %s' % (file_path, e))
+            return False
+    return True
--- src/main.py
+++ src/main.py
@@ -1,36 +1,9 @@
-from text_node import TextNode, TextType
-from markdown_to_html import markdown_to_html_node
+from copy_contents import copy_dir_contents
 
 def main():
-    text = \
-"""
-# Heading
-
-## Subheading
-
-- unordered list
-- another *italic* item
-- and another
-
-this is some **bold** paragraph text
-
-```
-def main():
-    print("Hello world!")
-    return 696969
-```
-
-> this is a quote
-> with multiple lines
-
-### this is a heading at the bottom
-
-1. with an ordered list in it
-2. which has some items
-"""
-    html_nodes = markdown_to_html_node(text)
-    print(html_nodes)
-
+    src = "static/"
+    dest = "public/"
+    copy_dir_contents(src, dest)
     
 
 
